b1c2aac1efe30f3d3b1e58ff9b32e6a3e18d8e53
Omarolc <88909440+Omarolc@users.noreply.github.com>
2026-03-30 17:20:07 -0600

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,23 +1,13 @@
-export default async function handler(req, res) {
-
-  const VERIFY_TOKEN = "ACR123";
+module.exports = (req, res) => {
 
   if (req.method === 'GET') {
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    if (mode && token === VERIFY_TOKEN) {
-      return res.status(200).send(challenge);
-    } else {
-      return res.sendStatus(403);
-    }
+    return res.status(200).send("ok");
   }
 
   if (req.method === 'POST') {
-    console.log("MENSAJE RECIBIDO:", JSON.stringify(req.body));
+    console.log("MENSAJE:", req.body);
     return res.status(200).send("ok");
   }
 
   return res.status(200).send("ok");
-}
+};
