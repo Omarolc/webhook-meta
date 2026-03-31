d429456fc11449d4a4fd54823b6473d80ee4639a
Omarolc <88909440+Omarolc@users.noreply.github.com>
2026-03-31 16:24:17 -0600

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,16 @@
-export default async function handler(req, res) {
+[3:16 p.m., 31/3/2026] padi olc: export default function handler(req, res) {
+  if (req.method === 'GET') {
+    return res.status(200).send('ok');
+  }
+
+  if (req.method === 'POST') {
+    console.log('Webhook recibido:', req.body);
+    return res.status(200).send('EVENT_RECEIVED');
+  }
+
+  res.status(405).send('Method Not Allowed');
+}
+[4:02 p.m., 31/3/2026] padi olc: export default async function handler(req, res) {
   try {
     if (req.method === 'GET') {
       return res.status(200).send('ok');
