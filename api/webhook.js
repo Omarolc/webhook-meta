34225e78cbdb916939e6e259143280bd305693a4
Omarolc <88909440+Omarolc@users.noreply.github.com>
2026-03-30 20:04:38 -0600

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,22 +1,21 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method === "POST") {
     try {
       const body = req.body;
 
       console.log("CUERPO COMPLETO:", JSON.stringify(body));
 
-      const entry = body.entry?.[0];
-      const change = entry?.changes?.[0];
-      const value = change?.value;
+      const entry = body.entry && body.entry[0];
+      const change = entry && entry.changes && entry.changes[0];
+      const value = change && change.value;
 
-      if (value?.messages) {
+      if (value && value.messages) {
         const message = value.messages[0];
         const from = message.from;
-        const text = message.text?.body;
+        const text = message.text && message.text.body;
 
         console.log("MENSAJE RECIBIDO:", text);
 
-        // RESPUESTA AUTOMÁTICA
         await fetch(
           https://graph.facebook.com/v18.0/${process.env.PHONE_NUMBER_ID}/messages,
           {
@@ -44,4 +43,4 @@ export default async function handler(req, res) {
   }
 
   return res.status(200).send("ok");
-}
+};
