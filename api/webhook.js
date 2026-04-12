39f9f1d87c4a015b2a01593fdb3e049e1ea854f0
Omarolc <88909440+Omarolc@users.noreply.github.com>
2026-04-11 20:18:21 -0600

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,7 +1,6 @@
 module.exports = async function handler(req, res) {
   const VERIFY_TOKEN = "ACR123";
 
-  // 🔹 VERIFICACIÓN
   if (req.method === "GET") {
     const mode = req.query["hub.mode"];
     const token = req.query["hub.verify_token"];
@@ -10,38 +9,31 @@ module.exports = async function handler(req, res) {
     if (mode === "subscribe" && token === VERIFY_TOKEN) {
       return res.status(200).send(challenge);
     } else {
-      return res.status(403).send("Error de verificación");
+      return res.status(403).send("Error");
     }
   }
 
-  // 🔹 MENSAJES
   if (req.method === "POST") {
     const body = req.body;
-
-    console.log("📩 Webhook:", JSON.stringify(body));
 
     const message =
       body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
 
     if (message) {
       const from = message.from;
-      const text = message.text?.body;
-
-      console.log("De:", from);
-      console.log("Texto:", text);
 
       await fetch(
         "https://graph.facebook.com/v18.0/1075972065598074/messages",
         {
           method: "POST",
           headers: {
-            Authorization: Bearer TU_TOKEN_AQUI,
+            Authorization: Bearer EAASGMfpSvMQBRN8IiSr3DIElZA4hb7qZCyxSnhIp0ruYu1OXcqrC7MvFfz3XJU5SYjHvhkdyn9JYpZBOEAeN424uhSuDJJmwZBNPvLIodqmDJYlHgTDs6sP3vtfcD2r9cwt1LnWgqLqjeorlhzW6CLbL2ca7FFdRQD1oKTmf8Jiws5FuqSPS0HZABMyfmEBidxoeyTT1o1sZAlKzqipPZAYkGdBluZCdzBBkaUFLBVlxc7bQsHKPP8ZBNwpA8g8yRoaQ7SxrDDIjL5oZBt1kZBVDlLgpZAvidgZDZD,
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
             messaging_product: "whatsapp",
             to: from,
-            text: { body: "Ya funciona tu bot 🔥" },
+            text: { body: "🔥 Ya quedó, responde el bot" },
           }),
         }
       );
@@ -50,5 +42,4 @@ module.exports = async function handler(req, res) {
     return res.status(200).send("EVENT_RECEIVED");
   }
 
-  return res.status(405).send("Método no permitido");
-};
+  return res
